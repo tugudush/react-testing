3d0311b2761d924a23668ec1c02ac9fc7c255a37
tugudush <jerome2kph@gmail.com>
2024-08-14 09:26:36 +0800

    toggle button color and test class

--- vite-starter/vite.config.js
+++ vite-starter/vite.config.js
@@ -16,6 +16,6 @@ export default defineConfig({
     // you might want to disable the `css: true` line, since we don't have
     // tests that rely on CSS -- and parsing CSS is slow.
     // I'm leaving it in here becasue often people want to parse CSS in tests.
-    css: true,
+    css: false,
   },
 });
