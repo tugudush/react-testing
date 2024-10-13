feabc0234af367f26a5cf1b57cdb15f89b76151a
tugudush <jerome2kph@gmail.com>
2024-10-13 11:33:47 +0800

    progress but with errors

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -1,3 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+
 export default function Options({ optionType }) {
-  return <div />;
+  const [items, setItems] = useState([]);
+
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch((err) => {
+        // TODO: handle error response
+        console.error(err);
+      });
+  }, [optionType]);
+
+  // TODO: replace `null` with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
 }
