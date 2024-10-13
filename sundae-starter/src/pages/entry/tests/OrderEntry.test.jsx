7f7b43d6b5ce9f1f8b29f1435546135480864047
tugudush <jerome2kph@gmail.com>
2024-10-13 23:48:55 +0800

    progress with intentional failed tests

---
+++ sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,23 @@
+import { http, HttpResponse } from "msw";
+import { server } from "../../../mocks/server";
+import { render, screen } from "@testing-library/react";
+import OrderEntry from "../OrderEntry";
+
+test("handles error for scoops and toppings routes", async () => {
+  server.resetHandlers(
+    http.get("http://localhost:3030/scoops", () => {
+      return new HttpResponse(null, { status: 500 });
+    }),
+    http.get("http://localhost:3030/toppings", () => {
+      return new HttpResponse(null, { status: 500 });
+    })
+  );
+
+  render(<OrderEntry />);
+
+  const alerts = await screen.findAllByRole("alert", {
+    name: "An unexpected error occurred. Please try again alert.",
+  });
+
+  expect(alerts).toHaveLength(2);
+});
